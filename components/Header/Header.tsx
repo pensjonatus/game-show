4e4089b5d2004ccc001af99da79dd5784c57300f
pensjonatus <pensjonatus@gmail.com>
2022-05-13 14:18:47 +0200

    tweaks to home page

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,36 +1,10 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
 import styles from './Header.module.css';
+import Left from './Left/Left';
 
 const Header: React.FC = () => {
-
-  const left = (
-    <div className={styles.left}>
-      <Link href="/">
-        <a>
-          <Image src="/logo.png" alt="logo" width="32" height="32" />
-        </a>
-      </Link>
-      <Link href="/game">
-        <a target="_blank" className="button">
-          Game
-        </a>
-      </Link>
-    </div>
-  );
-
-
   return (
-    <nav>
-      {left}
-      <style jsx>{`
-        nav {
-          display: flex;
-          padding: 2rem;
-          align-items: center;
-        }
-      `}</style>
+    <nav className={styles.nav}>
+      <Left />
     </nav>
   );
 };
---
+++ components/Header/Left/Left.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './Left.module.css';
+import { useGame } from '../../../lib/gameHooks';
+
+export default function Left() {
+  const { game, isError, isLoading } = useGame();
+
+  if (isError || isLoading) {
+    return null;
+  }
+
+  return (
+    <div className={styles.left}>
+      <Link href="/">
+        <a>
+          <Image src="/logo.png" alt="logo" width="32" height="32" />
+        </a>
+      </Link>
+      {game.inProgress ? (
+        <Link href="/game">
+          <a target="_blank" className="button">
+            Game
+          </a>
+        </Link>
+      ) : (
+        <button className="disabledButton">No game running</button>
+      )}
+    </div>
+  );
+}
