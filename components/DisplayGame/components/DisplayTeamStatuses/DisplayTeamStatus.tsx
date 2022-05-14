da33d3286863ae0ff0229f910d05c161f72cba66
pensjonatus <pensjonatus@gmail.com>
2022-05-14 13:40:21 +0200

    better status display

--- components/DisplayGame/components/DisplayTeamStatuses/DisplayTeamStatus.tsx
+++ components/DisplayGame/components/DisplayTeamStatuses/DisplayTeamStatus.tsx
@@ -3,12 +3,11 @@ import LostChances from '../../../LostChances/LostChances';
 import styles from './DisplayTeamStatuses.module.css';
 
 export default function DisplayTeamStatus({ team }: { team: Team }) {
-
   return (
     <div className={styles.team}>
       <div className={styles.score}>{team.score}</div>
       <div className={styles.name}>{team.name}</div>
-      <div>
+      <div className={styles.lostChances}>
         <LostChances howMany={team.chancesLost} playSound={true} />
       </div>
     </div>
