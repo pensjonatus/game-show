516865377b1d1d7fe3f218d75970cc61f8e9c908
pensjonatus <pensjonatus@gmail.com>
2022-05-15 16:04:20 +0200

    use badges for team display

--- components/DisplayGame/components/DisplayTeamStatuses/DisplayTeamStatus.tsx
+++ components/DisplayGame/components/DisplayTeamStatuses/DisplayTeamStatus.tsx
@@ -1,5 +1,6 @@
 import { Team } from '@prisma/client';
-import LostChances from '../../../LostChances/LostChances';
+import { useEffect, useRef, useState } from 'react';
+import Badges from '../../../Badges/Badges';
 import styles from './DisplayTeamStatuses.module.css';
 
 export default function DisplayTeamStatus({ team }: { team: Team }) {
@@ -7,9 +8,12 @@ export default function DisplayTeamStatus({ team }: { team: Team }) {
     <div className={styles.team}>
       <div className={styles.score}>{team.score}</div>
       <div className={styles.name}>{team.name}</div>
-      <div className={styles.lostChances}>
-        <LostChances howMany={team.chancesLost} playSound={true} />
-      </div>
+      <Badges
+        badge="😒"
+        howMany={team.chancesLost}
+        playSound={true}
+        className={styles.lostChances}
+      />
     </div>
   );
 }
